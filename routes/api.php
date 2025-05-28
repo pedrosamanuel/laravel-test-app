fccb0b3e97f6cf7be0cf30937654c22825029a57
Manuel Pedrosa <mpedrosa2110@gmail.com>
2025-05-28 12:50:09 -0300

    finished

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*Route::get('/user', function (Request $request) {
+Route::get('/me', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');*/
+})->middleware('auth:sanctum');
 
 Route::apiResource('product', ProductController::class);
 
